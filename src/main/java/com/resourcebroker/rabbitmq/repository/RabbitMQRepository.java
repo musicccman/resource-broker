d3a06d9fa00b2af9fa1e95b0e0b2772f925d6a79
musicccman <akozlov.rfe@gmail.com>
2015-04-24 15:30:16 +0300

    error handling added

--- src/main/java/com/resourcebroker/rabbitmq/repository/RabbitMQRepository.java
+++ src/main/java/com/resourcebroker/rabbitmq/repository/RabbitMQRepository.java
@@ -1,6 +1,5 @@
 package com.resourcebroker.rabbitmq.repository;
 
-import com.resourcebroker.common.config.RabbitMQConfig;
 import com.resourcebroker.common.entitiy.ServiceUserEntity;
 import com.resourcebroker.common.exception.ApplicationException;
 import com.resourcebroker.common.repository.ServiceInstanceRepository;
@@ -15,7 +14,6 @@ import org.springframework.stereotype.Repository;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 
 /**
  * @author: Andrey Kozlov
@@ -25,8 +23,6 @@ public class RabbitMQRepository implements ServiceInstanceRepository {
 
     Logger logger = Logger.getRootLogger();
 
-    @Autowired
-    private RabbitMQConfig rabbitMQConfig;
     @Autowired
     private RequestBuilder requestBuilder;
 
@@ -40,23 +36,8 @@ public class RabbitMQRepository implements ServiceInstanceRepository {
     @Override
     public String createService(String serviceName) throws ApplicationException{
         HttpRequestBase requestBase = requestBuilder.getVhostCreateRequest(serviceName);
-        executeRequest(serviceName, requestBase);
+        executeRequest(requestBase);
         return serviceName;
-    }
-
-    private void executeRequest(String serviceName, HttpRequestBase requestBase) throws ApplicationException{
-        try (CloseableHttpResponse response = httpClient.execute(requestBase)) {
-            handleResponce(response);
-        } catch (IOException e) {
-            ApplicationException.fail(e);
-        }
-    }
-
-    private void handleResponce(CloseableHttpResponse response) throws ApplicationException{
-        int responceCode = response.getStatusLine().getStatusCode();
-        if (!((responceCode>199)&&(responceCode<299))){ //not 2.. (sucess responce)
-            ApplicationException.fail(response.getStatusLine().toString());
-        }
     }
 
     @Override
@@ -78,6 +59,21 @@ public class RabbitMQRepository implements ServiceInstanceRepository {
     @Override
     public void deleteServiceUser(ServiceUserEntity serviceUser) {
 
+    }
+
+    private void executeRequest(HttpRequestBase requestBase) throws ApplicationException{
+        try (CloseableHttpResponse response = httpClient.execute(requestBase)) {
+            handleResponce(response);
+        } catch (IOException e) {
+            ApplicationException.fail(e);
+        }
+    }
+
+    private void handleResponce(CloseableHttpResponse response) throws ApplicationException{
+        int responceCode = response.getStatusLine().getStatusCode();
+        if (!((responceCode>199)&&(responceCode<299))){ //not 2.. (sucess responce)
+            ApplicationException.fail(response.getStatusLine().toString());
+        }
     }
 
 }
